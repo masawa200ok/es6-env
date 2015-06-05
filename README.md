c917ddd5f4eb0aa4746ac3f714ea98371091883e
masawa200ok <masawa200ok@MasawaPro.local>
2015-06-05 19:56:45 +0900

    add browserify

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # es6-env
 to es6, npm install !!!
+
+## npm run gulp
+
+es6を想定したファイルをgulpでウォッチしes5へトランスパイル．
+
+## browserify
+
+gulpでbrowserifyしたかったけどやり方がわからない．
+
+結局手動でbrowserify．
+
+    browserify <src> -o <dest>
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,11 @@
-var gulp = require("gulp");
-/*
-var sourcemaps = require("gulp-sourcemaps");
-*/
-var babel = require("gulp-babel");
-/*
-var concat = require("gulp-concat");
-*/
-gulp.task("default", function () {
-  return gulp.src("es6/**/*.js")
-    /*
-    .pipe(sourcemaps.init())
-    .pipe(concat("all.js"))
-    */
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var src_dir = './es6/**/*.js'
+gulp.task('default', function () {
+  return gulp.src(src_dir)
     .pipe(babel())
-    /*
-    .pipe(sourcemaps.write("."))
-    */
-    .pipe(gulp.dest("js"));
+    .pipe(gulp.dest('js'));
 });
-
-gulp.task("watch", function() {
-  gulp.watch("es6/**/*.js", ["default"]);
+gulp.task('watch', function(){
+    gulp.watch(src_dir, ['default']);
 });
